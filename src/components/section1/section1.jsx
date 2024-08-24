044f191e069dd130c2ec154e4b6979ba2721c90f
MQuirico <128657405+MQuirico@users.noreply.github.com>
2024-08-24 16:57:00 -0300

    section 1 finalizada

---
+++ src/components/section1/section1.jsx
@@ -0,0 +1,26 @@
+import picture from "../../assets/hero-bg.jpg"
+import "./section1.css"
+
+export default function Section1 (){
+    return(
+        <>
+           <div className="section1">
+                <section className="textArea">
+                    <h2 className="yourGuide">
+                        YOUR GUIDE TO <br></br>
+                        QUALITY COVERAGE
+                    </h2>
+                    <p className="text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing<br></br>
+                        elit, sed do eiusmod tempor incididunt ut labore et<br></br>
+                        dolore magna aliqua.
+                    </p>
+                    <button className="findPlans">
+                        FIND PLANS
+                    </button>
+                </section>
+                <img src={picture}></img>
+           </div> 
+        </>
+    )
+}
--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/homePage/header/header"
+import Section1 from "../../components/section1/section1"
 import "./homePage.css"
 
 export default function HomePage (){
     return(
         <div className="home">
-           <Header />         
+           <Header />  
+           <Section1 />       
         </div>
     )
 }
