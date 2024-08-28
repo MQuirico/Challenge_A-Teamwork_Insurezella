c7332f26bb9f68e9b3cef86b183471742b462ad1
MQuirico <128657405+MQuirico@users.noreply.github.com>
2024-08-28 01:32:46 -0300

    header version mobile completa

---
+++ src/components/homePage/header/drawer (mobile)/drawer.jsx
@@ -0,0 +1,54 @@
+import { useState, Fragment } from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+import { ThemeProvider } from '@emotion/react';
+
+export default function TemporaryDrawer() {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {['Home', 'Find insurance', 'Learn', 'About'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <MenuIcon 
+      onClick={toggleDrawer(true)}
+      sx={{
+        position: "absolute",
+        left: "17%",
+        top: "25%",
+        maxWidth: "70%"
+        
+      }}
+      />
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+}
--- src/components/homePage/header/header.jsx
+++ src/components/homePage/header/header.jsx
@@ -1,12 +1,16 @@
 import Logo from "../../../assets/logo.png"
 import Sections from "./sections/sections"
+import { useMediaQuery } from '@mui/material';
+import TemporaryDrawer from "./drawer (mobile)/drawer";
 import "./header.css"
 
 export default function Header (){
+    const isMobile = useMediaQuery('(min-width:300px)') && useMediaQuery('(max-width: 840px)')
     return(
         <>
-        <header>
+        <header className="header">
             <div className="lightBlue">
+                {isMobile && <TemporaryDrawer sx={{ color: "black"}} />}
                 <h1 className="connect">Want to connect with us?</h1>
                 <h3 className="mail">📧Mail us: support@insurezella.com</h3>
                 <h3 className="phone">&#128222; Call us: (877) 227-0774</h3>
